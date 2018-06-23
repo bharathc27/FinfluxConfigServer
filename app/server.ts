a7fd8d84e4faafdddce6326387217e009b338c5a
kmkanagu <kmkanagu@gmail.com>
2018-06-23 17:35:35 +0530

    configured default server prot to 8080

--- app/server.ts
+++ app/server.ts
@@ -1,6 +1,6 @@
 import {App} from './app'
 
-const port: any = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+const port: any = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const server_ip_address: string = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 const app: App = new App(server_ip_address, port);
--- build/server.js
+++ build/server.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var app_1 = require("./app");
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var app = new app_1.App(server_ip_address, port);
 app.startServer();
